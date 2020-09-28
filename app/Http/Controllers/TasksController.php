321375da56299ac6a02e599a7be7ce874b7a3e73
Ubuntu <ubuntu@ip-172-31-81-55.ec2.internal>
2020-09-28 10:24:30 +0000

    kadai-lesson13-1

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -116,7 +116,7 @@ class TasksController extends Controller
     public function destroy($id)
     {
 
-        $task = Message::findOrFail($id);
+        $task = Task::findOrFail($id);
 
         $task->delete();
 
